17aaf1e1ca272d10a54705397003046c6b77ee6b
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2017-11-29 10:42:27 +0100

    refactored render method in liquid_block_extensions

--- lib/japr/extensions/liquid/liquid_block_extensions.rb
+++ lib/japr/extensions/liquid/liquid_block_extensions.rb
@@ -12,30 +12,36 @@ module JAPR
 
     def render(context)
       site = context.registers[:site]
-      config = site.config['asset_pipeline'] || {}
+      config = site.config.fetch('asset_pipeline', {})
 
       # Run Jekyll Asset Pipeline
-      pipeline, cached = Pipeline.run(nodelist.first, @markup.strip,
-                                      site.source, site.dest,
-                                      self.class.tag_name,
-                                      self.class.output_type, config)
+      pipeline, cached = run_pipeline(site, config)
 
       return nil unless pipeline.is_a?(Pipeline)
 
-      unless cached
-        # Prevent Jekyll from cleaning up saved assets if new pipeline
-        pipeline.assets.each do |asset|
-          config = JAPR::DEFAULTS.merge(config)
-          staging_path = File.expand_path(File.join(site.source,
-                                                    config['staging_path']))
-          site.static_files << Jekyll::StaticFile.new(site, staging_path,
-                                                      asset.output_path,
-                                                      asset.filename)
-        end
-      end
+      # Prevent Jekyll from cleaning up saved assets if new pipeline
+      preserve_assets(site, config, pipeline) unless cached
 
       # Return HTML tag pointing to asset
       pipeline.html
+    end
+
+    private
+
+    def run_pipeline(site, config)
+      Pipeline.run(nodelist.first, @markup.strip, site.source, site.dest,
+                   self.class.tag_name, self.class.output_type, config)
+    end
+
+    def preserve_assets(site, config, pipeline)
+      pipeline.assets.each do |asset|
+        config = JAPR::DEFAULTS.merge(config)
+        staging_path = File.expand_path(File.join(site.source,
+                                                  config['staging_path']))
+        site.static_files << Jekyll::StaticFile.new(site, staging_path,
+                                                    asset.output_path,
+                                                    asset.filename)
+      end
     end
   end
 end
